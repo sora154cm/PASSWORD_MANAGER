26505242cfd3711de9d2f34e6178a0f237a09aa4
Takashi Sato <ahiru154cm@gmail.com>
2023-05-07 23:40:02 +0900

    ステップ1の完了

--- account_information.sh
+++ account_information.sh
@@ -1,24 +1,57 @@
 #!/bin/bash
 
 #追記するファイル名（テキストファイル)を指定する
-information = "information.txt"
+#変数を定義するときにはイコールの前後にスペースがないことに注意
+information="information.txt"
 
 echo "パスワードマネージャーへようこそ！"
 
-echo "サービス名を入力してください："
-read service_name
-#>> 演算子を使いリダイレクトでファイルにサービス名を追記
-echo "$service_name" >> "$information"
+#条件がtrueであるかぎり処理をループ
+while true; do
+  # -pオプションで入力前にメッセージを設定
+  read -p "サービス名を入力してください：" service_name
 
-echo "ユーザー名を入力してください："
-read user_name
-#ファイルにユーザー名を追記
-echo "$user_name" >> "$information"
+  if [[ -z "$service_name" ]]; then
+    #service_nameが空である場合
+    echo "サービス名が空欄となっております"
+  else
+    #>> から出ない場合、演算子を使いリダイレクトでファイルにサービス名を追記
+    echo "サービス名: $service_name" >> "$information"
+    #入力が確認できたら処理を終了する
+    break
+  fi
+done
 
-echo "パスワードを入力してください："
-read password
-#パスワードを追記
-echo "$password" >> "$information"
+
+while true; do
+  read -p "ユーザー名を入力してください：" user_name
+
+  if [[ -z "$user_name" ]]; then
+    #service_nameが空である場合
+    echo "サービス名が空欄となっております"
+  else
+    #ファイルにユーザー名を追記
+    echo "ユーザー名: $user_name" >> "$information"
+    #入力が確認できたら処理を終了する
+    break
+  fi
+done
+
+while true; do
+  read -p "パスワードを入力してください：" password
+
+  if [[ -z "$password" ]]; then
+    #service_nameが空である場合
+    echo "サービス名が空欄となっております"
+  else
+    #パスワードを追記
+    echo "パスワード: $password" >> "$information"
+    #入力が確認できたら処理を終了する
+    break
+  fi
+done
 
 # 入力が完了したら
+#区切り線も入れとく
+echo "ーーーーーーーーーーーーーーーーーーーーーー" >> "$information"
 echo "Thank you!"
